0938f490dd61ccc46e4f04d729e5ea8902242279
timgold81 <timgold@gmail.com>
2018-04-03 13:40:10 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Telnet honeypot
+<pre>
+
+usage: honeypot.py [-h] [-p PORT] [-f FILENAME] [-u USERNAME] [-w PASSWORD]
+                   [-o OUTPUT] [-I IP] [-a ATTEMPTS] [-d DENIED]
+
+Simple honey pod for telnet CLI network connections attempts
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  Port number to listen for connections. Default: 23
+  -f FILENAME, --filename FILENAME
+                        Text file name with welcome message to be sent to the
+                        attacker. Default: No filename fill be loaded and no
+                        message will be displayed
+  -u USERNAME, --username USERNAME
+                        Promt that will be sent to attacker to ask for
+                        username. Default: "Username:"
+  -w PASSWORD, --password PASSWORD
+                        Promt that will be sent to attacker to ask for
+                        password. Default: "Password:"
+  -o OUTPUT, --output OUTPUT
+                        Output file to log login attempts. Default: output.log
+  -I IP, --ip IP        Bind to specific IP. Default: localhost
+  -a ATTEMPTS, --attempts ATTEMPTS
+                        Connections attempts counter. Default: 5
+  -d DENIED, --denied DENIED
+                        Fail message to send to attacker. Default: "Access
+                        denied"
+
+</pre>
+
+## Usage
+To use forward connection from router to the port using this program.
+
+## Example
+I started the program on port 999 using :
+<pre>
+sudo ./honeypot.py -p 999 -f welcome.txt
+</pre>
+And I forwarded ports 22 and 23 from my router to port 999 of the computer
+(Raspberry pi) After some time the output file output.log contained the
+following:
+<pre>
+2018-04-03 13:03:33.436315 Logged in: ('82.80.148.44', 56405)
+2018-04-03 13:03:33.966303, Username recieved: root
+2018-04-03 13:03:34.488361, Password recieved: hunt5759
+2018-04-03 13:03:35.007603, Username recieved: root
+2018-04-03 13:03:35.529605, Password recieved: default
+2018-04-03 13:03:36.054998, Username recieved: guest
+2018-04-03 13:03:36.583650, Password recieved: 12345
+2018-04-03 13:03:37.111977, Username recieved: root
+2018-04-03 13:03:37.633298, Password recieved: ttnet
+2018-04-03 13:03:37.633749 Logged in: ('122.226.181.167', 51034)
+2018-04-03 13:03:38.134534 Connection closed
+2018-04-03 13:03:38.134756 Logged in: ('82.80.148.44', 56429)
+2018-04-03 13:03:38.655134, Username recieved: supervisor
+2018-04-03 13:03:39.179816, Password recieved: supervisor
+2018-04-03 13:03:39.699053, Username recieved: root
+2018-04-03 13:03:40.237234, Password recieved: aquario
+2018-04-03 13:03:40.755778, Username recieved: admin
+2018-04-03 13:03:41.275820, Password recieved: pass
+2018-04-03 13:03:41.795914, Username recieved: root
+2018-04-03 13:03:42.315545, Password recieved: klv1234
+2018-04-03 13:03:42.356125 Logged in: ('82.80.148.44', 56459)
+2018-04-03 13:03:42.881175, Username recieved: root
+2018-04-03 13:03:43.420037, Password recieved: hi3518
+2018-04-03 13:03:43.942748, Username recieved: root
+2018-04-03 13:03:44.465876, Password recieved: user
+2018-04-03 13:03:44.986794, Username recieved: root
+2018-04-03 13:03:45.508611, Password recieved: 1234567890
+2018-04-03 13:03:46.028584, Username recieved: admin
+2018-04-03 13:03:46.551995, Password recieved: 1111111
+</pre>
+These are only few lines from the output file out of many
---
+++ honeypot.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+import socket
+import sys
+import argparse
+import signal
+from datetime import datetime
+import time
+import errno
+
+class Configuration:
+    port=23
+    filename=""
+    username="Username:"
+    password="Password:"
+    message="\n"
+    output="output.log"
+    ip="0.0.0.0"
+    attempts=5
+    denied="Access denied\n"
+
+    def load_message(self):
+        if self.filename!="":
+            file=open(self.filename,'r')
+            self.message=file.read()
+            file.close()
+        else:
+            print ("no filename is loaded")
+    def log_login_attempt(self,output):
+        file=open(self.output,"a")
+        file.write(output+"\n")
+        file.close()
+
+config = Configuration()
+
+def signal_handler(signal,frame):
+    global loop
+    loop=0
+    global connection
+    connection.close()
+    print ("\nStopping pinger. Thanks for using.")
+    print ("Please visit https://github.com/timgold81/")
+    print ("contact timgold@gmail.com\n")
+
+
+signal.signal(signal.SIGINT,signal_handler)
+
+parser=argparse.ArgumentParser(description="Simple honey pod for telnet CLI net\
+work connections attempts")
+parser.add_argument("-p","--port",help="Port number to listen for connections. \
+Default: 23")
+parser.add_argument("-f","--filename",help="Text file name with welcome message\
+ to be sent to the attacker. Default: No filename fill be loaded and no message\
+ will be displayed")
+parser.add_argument("-u","--username",help="Promt that will be sent to attacker\
+ to ask for username. Default: \"Username:\"")
+parser.add_argument("-w","--password",help="Promt that will be sent to attacker\
+ to ask for password. Default: \"Password:\"")
+parser.add_argument("-o","--output",help="Output file to log login attempts. \
+Default: output.log")
+parser.add_argument("-I","--ip",help="Bind to specific IP. Default: localhost")
+parser.add_argument("-a","--attempts",help="Connections attempts counter. \
+Default: 5")
+parser.add_argument("-d","--denied",help="Fail message to send to attacker. \
+Default: \"Access denied\"")
+args=parser.parse_args()
+
+if args.port:
+    config.port=int(args.port)
+
+if args.filename:
+    config.filename=args.filename
+    config.load_message()
+
+if args.username:
+    config.username=args.username+"\n"
+
+if args.password:
+    config.password=args.password++"\n"
+
+if args.output:
+    config.output=args.Output
+
+if args.ip:
+    config.ip=args.ip
+
+if args.attempts:
+    config.attempts=int(args.attempts)
+
+if args.denied:
+    config.denied=args.denied +"\n"
+
+loop=1
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind((config.ip,config.port))
+sock.listen(1)
+while loop:
+    connection, client_addr=sock.accept()
+    config.log_login_attempt(str(datetime.now())+" Logged in: "+str(client_addr)+"\n")
+    connection.sendall(config.message)
+    try:
+        for i in range(1,config.attempts):
+            time.sleep(0.5)
+            connection.sendall(config.username)
+            data=connection.recv(255)
+            if data:
+                config.log_login_attempt(str(datetime.now())+", Username recieved: "+data)
+            else:
+                break
+            time.sleep(0.5)
+            connection.sendall(config.password)
+            data=connection.recv(255)
+            if data:
+                config.log_login_attempt(str(datetime.now())+", Password recieved: "+data)
+                connection.sendall(config.denied)
+            else:
+                break
+        connection.close()
+    except socket.error as e:
+        config.log_login_attempt(str(datetime.now())+" Connection closed")
+        connection.close()
+    finally:
+        connection.close()
